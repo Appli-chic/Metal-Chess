63ee21bcf34d76e7fa90828b55aa7a142b3d4a09
Guillaume Belouin <gbelouin@protonmail.com>
2026-03-30 09:49:00 +0200

    Initial Commit

---
+++ Metal Chess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Metal Chess
+//
+//  Created by Guillaume Belouin on 30/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Metal Chess/Metal_ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_ChessApp.swift
+//  Metal Chess
+//
+//  Created by Guillaume Belouin on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Metal_ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
