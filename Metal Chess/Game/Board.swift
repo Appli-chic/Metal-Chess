7eae0846f0409db9c1deacd5eea2b264fd8432cd
Guillaume Belouin <gbelouin@protonmail.com>
2026-04-02 15:08:28 +0200

    Remove the Tile and optimize the GPU resource by creating all the vertices in the board directly and only one send to the GPU

--- Metal Chess/Game/Board.swift
+++ Metal Chess/Game/Board.swift
@@ -3,60 +3,43 @@ import MetalKit
 private let BOARD_SIZE = 8
 
 class Board {
-    private var tiles: [[Tile]] = []
+    private let vertexBuffer: MTLBuffer
+    private var vertexSize: Int = 0
     
     init(metalDevice: MTLDevice) {
-        for y in 0..<BOARD_SIZE {
-            var tileRow: [Tile] = []
-            
-            for x in 0..<BOARD_SIZE {
-                tileRow.append(Tile(metalDevice: metalDevice, x: x, y: y))
-            }
-            
-            tiles.append(tileRow)
-        }
-    }
-    
-    func draw(renderEncoder: MTLRenderCommandEncoder?) {
-        for row in tiles {
-            for tile in row {
-                tile.draw(renderEncoder: renderEncoder)
-            }
-        }
-    }
-}
-
-class Tile {
-    private let vertexBuffer: MTLBuffer
-    
-    init(metalDevice: MTLDevice, x: Int, y: Int) {
         let black: vector_float4 = [0, 0, 0, 1]
         let white: vector_float4 = [1, 1, 1, 1]
-        
-        let color: vector_float4 = if (x + y) % 2 == 0 {
-            black
-        } else {
-            white
-        }
-        
         let tileSize: Float = 2 / Float(BOARD_SIZE)
-        let positionX = -1 + (tileSize * Float(x))
-        let positionY = -1 + (tileSize * Float(y))
+        var vertices: [Vertex] = []
         
-        let bottomLeftVertex = Vertex(position: [positionX, positionY], color: color)
-        let bottomRightVertex = Vertex(position: [positionX + tileSize, positionY], color: color)
-        let topRightVertex = Vertex(position: [positionX + tileSize, positionY + tileSize], color: color)
-        let topLeftVertex = Vertex(position: [positionX, positionY + tileSize], color: color)
-        
-        let vertices = [
-            topLeftVertex,
-            topRightVertex,
-            bottomRightVertex,
-            
-            topLeftVertex,
-            bottomLeftVertex,
-            bottomRightVertex,
-        ]
+        for y in 0..<BOARD_SIZE {
+            for x in 0..<BOARD_SIZE {
+                let color: vector_float4 = if (x + y) % 2 == 0 {
+                    black
+                } else {
+                    white
+                }
+                
+                let positionX = -1 + (tileSize * Float(x))
+                let positionY = -1 + (tileSize * Float(y))
+                
+                let bottomLeftVertex = Vertex(position: [positionX, positionY], color: color)
+                let bottomRightVertex = Vertex(position: [positionX + tileSize, positionY], color: color)
+                let topRightVertex = Vertex(position: [positionX + tileSize, positionY + tileSize], color: color)
+                let topLeftVertex = Vertex(position: [positionX, positionY + tileSize], color: color)
+                
+                self.vertexSize += 6;
+                vertices.append(contentsOf: [
+                    topLeftVertex,
+                    topRightVertex,
+                    bottomRightVertex,
+                    
+                    topLeftVertex,
+                    bottomLeftVertex,
+                    bottomRightVertex,
+                ])
+            }
+        }
         
         self.vertexBuffer = metalDevice.makeBuffer(
             bytes: vertices,
@@ -67,6 +50,6 @@ class Tile {
     
     func draw(renderEncoder: MTLRenderCommandEncoder?) {
         renderEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 6)
+        renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: self.vertexSize)
     }
 }
