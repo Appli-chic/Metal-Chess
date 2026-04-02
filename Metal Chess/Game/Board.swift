69a0fccff9cc84ae5fd6f6379ccca2e492538f37
Guillaume Belouin <gbelouin@protonmail.com>
2026-04-02 15:20:35 +0200

    Improve the performances using an inout [Vertex]

--- Metal Chess/Game/Board.swift
+++ Metal Chess/Game/Board.swift
@@ -7,37 +7,13 @@ class Board {
     private let vertexCount = BOARD_SIZE * BOARD_SIZE * 6
     
     init(metalDevice: MTLDevice) {
-        let black: vector_float4 = [0, 0, 0, 1]
-        let white: vector_float4 = [1, 1, 1, 1]
         let tileSize: Float = 2 / Float(BOARD_SIZE)
         var vertices: [Vertex] = []
         vertices.reserveCapacity(vertexCount)
         
         for y in 0..<BOARD_SIZE {
             for x in 0..<BOARD_SIZE {
-                let color: vector_float4 = if (x + y) % 2 == 0 {
-                    black
-                } else {
-                    white
-                }
-                
-                let positionX = -1 + (tileSize * Float(x))
-                let positionY = -1 + (tileSize * Float(y))
-                
-                let bottomLeftVertex = Vertex(position: [positionX, positionY], color: color)
-                let bottomRightVertex = Vertex(position: [positionX + tileSize, positionY], color: color)
-                let topRightVertex = Vertex(position: [positionX + tileSize, positionY + tileSize], color: color)
-                let topLeftVertex = Vertex(position: [positionX, positionY + tileSize], color: color)
-                
-                vertices.append(contentsOf: [
-                    topLeftVertex,
-                    topRightVertex,
-                    bottomRightVertex,
-                    
-                    topLeftVertex,
-                    bottomLeftVertex,
-                    bottomRightVertex,
-                ])
+                Board.createTileVertices(vertices: &vertices, x: x, y: y, tileSize: tileSize)
             }
         }
         
@@ -51,5 +27,38 @@ class Board {
     func draw(renderEncoder: MTLRenderCommandEncoder?) {
         renderEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
         renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertexCount)
+    }
+    
+    private static func createTileVertices(vertices: inout [Vertex], x: Int, y: Int, tileSize: Float) {
+        let color = Board.getTileColor(x: x, y: y)
+        
+        let positionX = -1 + (tileSize * Float(x))
+        let positionY = -1 + (tileSize * Float(y))
+        
+        let bottomLeftVertex = Vertex(position: [positionX, positionY], color: color)
+        let bottomRightVertex = Vertex(position: [positionX + tileSize, positionY], color: color)
+        let topRightVertex = Vertex(position: [positionX + tileSize, positionY + tileSize], color: color)
+        let topLeftVertex = Vertex(position: [positionX, positionY + tileSize], color: color)
+        
+        vertices.append(contentsOf: [
+            topLeftVertex,
+            topRightVertex,
+            bottomRightVertex,
+            
+            topLeftVertex,
+            bottomLeftVertex,
+            bottomRightVertex,
+        ])
+    }
+    
+    private static func getTileColor(x: Int, y: Int) -> vector_float4 {
+        let black: vector_float4 = [0, 0, 0, 1]
+        let white: vector_float4 = [1, 1, 1, 1]
+        
+        return if (x + y) % 2 == 0 {
+            black
+        } else {
+            white
+        }
     }
 }
